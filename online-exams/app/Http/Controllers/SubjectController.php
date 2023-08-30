337ad1ac9d356e481c35e77520c509457a0456a8
Zrin <zrin.crnjac@gmail.com>
2023-08-30 23:30:01 +0200

    Add CRUD methods for Tasks and Subjects

--- online-exams/app/Http/Controllers/SubjectController.php
+++ online-exams/app/Http/Controllers/SubjectController.php
@@ -3,8 +3,64 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Subject;
+use App\Models\User;
 
 class SubjectController extends Controller
 {
-    //
+    public function index()
+    {
+        $subjects = Subject::all();
+        return view('subjects.index', compact('subjects'));
+    }
+
+    public function create()
+    {
+        return view('subjects.create');
+    }
+
+    public function store(Request $request)
+    {
+        /** @var User $user */
+        $user = auth()->user();
+
+        if($user->can('create-subject')) {
+            $subject = new Subject([
+                'name' => $request->get('name'),
+                'description' => $request->get('description'),
+            ]);
+            $user->subjects()->save($subject);
+
+            return redirect('subjects.index')->with('success', 'Subject saved!');
+        } else {
+            return redirect('subjects.index')->with('error', 'You are not allowed to create a subject!');
+        }
+    }
+
+    public function show(Subject $subject)
+    {
+        return view('subjects.show', compact('subject'));
+    }
+
+    public function edit($id)
+    {
+        return view('subjects.edit');
+    }
+
+    public function update(Request $request, Subject $subject)
+    {
+        $subject->update([
+            'name' => $request->get('name'),
+            'description' => $request->get('description'),
+        ]);
+
+        return redirect()->route('subjects.index')->with('success', 'Subject updated successfully!');
+    }
+    
+    public function destroy(Subject $subject)
+    {
+        $subject->delete();
+
+        return redirect()->route('subjects.index')->with('success', 'Subject deleted successfully!');
+    }
 }
--- online-exams/app/Http/Controllers/TaskController.php
+++ online-exams/app/Http/Controllers/TaskController.php
@@ -3,8 +3,59 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Task;
 
 class TaskController extends Controller
 {
-    //
+    public function index()
+    {
+        $tasks = Task::all();
+        return view('tasks.index', compact('tasks'));
+    }
+
+    public function create()
+    {
+        return view('tasks.create');
+    }
+
+    public function store(Request $request){
+        $task = new Task([
+            'name' => $request->get('name'),
+            'description' => $request->get('description'),
+            'picture' => $request->get('picture'),
+            'subject_id' => $request->get('subject_id'),
+        ]);
+        $task->save();
+
+        return redirect('tasks.index')->with('success', 'Task saved!');
+    }
+
+    public function show(Task $task)
+    {
+        return view('tasks.show', compact('task'));
+    }
+
+    public function edit($id)
+    {
+        return view('tasks.edit');
+    }
+
+    public function update(Request $request, Task $task)
+    {
+        $task->update([
+            'name' => $request->get('name'),
+            'description' => $request->get('description'),
+            'picture' => $request->get('picture'),
+            'subject_id' => $request->get('subject_id'),
+        ]);
+
+        return redirect()->route('tasks.index')->with('success', 'Task updated successfully!');
+    }
+
+    public function destroy(Task $task)
+    {
+        $task->delete();
+
+        return redirect()->route('tasks.index')->with('success', 'Task deleted successfully!');
+    }
 }
